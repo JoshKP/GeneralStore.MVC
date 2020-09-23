c0a2cf7b473f490929a405c3e2ce61c92f021e25
Josh Piersimoni <68341491+JoshKP@users.noreply.github.com>
2020-09-23 14:46:07 -0400

    Add project files.

---
+++ GeneralStore.MVC/Controllers/TransactionController.cs
@@ -0,0 +1,127 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class TransactionController : Controller
+    {
+        // Add application DB Context (link to database)
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            List<Transaction> transactionList = _db.Transactions.ToList();
+            List<Transaction> orderedList = transactionList.OrderBy(t => t.TransactionID).ToList();
+            return View(orderedList);
+        }
+        // GET: Transaction
+        public ActionResult Create()
+        {
+            var products = new SelectList(_db.Products.ToList(), "ProductID", "Name");
+            ViewBag.Products = products;
+            var customers = new SelectList(_db.Customers.ToList(), "CustomerID", "FullName");
+            ViewBag.Customers = customers;
+            return View();
+        }
+
+        // POST: Transaction
+        [HttpPost]
+        public ActionResult Create(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                Customer customer = _db.Customers.Find(transaction.CustomerID);
+                if (customer == null)
+                    return HttpNotFound("Customer not found");
+
+                Product product = _db.Products.Find(transaction.ProductID);
+                if (product == null)
+                    return HttpNotFound("Product not found");
+
+                if (transaction.Quantity > product.InventoryCount)
+                    return HttpNotFound("There isn't enough product for this purchase");
+
+                _db.Transactions.Add(transaction);
+                product.InventoryCount -= transaction.Quantity;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(transaction);
+        }
+        // GET: Edit
+        // Transaction/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = _db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+        // POST: Edit
+        // Transaction/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(transaction).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(transaction);
+        }
+        // GET: Delete
+        // Product/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = _db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+        // POST: Delete
+        // Product/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Transaction transaction = _db.Transactions.Find(id);
+            _db.Transactions.Remove(transaction);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        // GET: Details
+        // Product/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = _db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+    }
+}
---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Customer))]
+        public int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Product))]
+        public int ProductID { get; set; }
+        public virtual Product Product { get; set; }
+
+        [Required]
+        [Display(Name = "Quantity Purchased")]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Date of transaction")]
+        public DateTime TransactionDate { get; set; }
+    }
+}
